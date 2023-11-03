0a40d0eeeb9d788c080c1a56229dc271118d4b22
tanyusheng <tanyusheng@outlook.com>
2023-11-04 00:11:03 +0800

    【first init】【Camera2 API】 AndroidStudio Project Camera Demo
    Signed-off-by: tanyusheng <tanyusheng@outlook.com>

---
+++ app/src/main/java/cn/yusheng123/xycamera/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.yusheng123.xycamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
