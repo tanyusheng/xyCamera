8528d00fa716e13457b08fc8579d6ac15a2647aa
tanyusheng <tanyusheng@outlook.com>
2023-11-10 01:09:58 +0800

    界面

--- app/src/main/java/cn/yusheng123/xycamera/MainActivity.java
+++ app/src/main/java/cn/yusheng123/xycamera/MainActivity.java
@@ -3,12 +3,31 @@ package cn.yusheng123.xycamera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.SparseIntArray;
+import android.view.Surface;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final SparseIntArray PHOTO_ORI = new SparseIntArray();
+
+    static {
+        PHOTO_ORI.append(Surface.ROTATION_0, 90);
+        PHOTO_ORI.append(Surface.ROTATION_90, 0);
+        PHOTO_ORI.append(Surface.ROTATION_180, 270);
+        PHOTO_ORI.append(Surface.ROTATION_270, 180);
+    }
+
+    int cameraOri;
+    int displayRotation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private void initCamera(){
+
     }
 }
